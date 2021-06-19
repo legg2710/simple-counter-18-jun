f65efe2d2b4080b4fb761afd5eaa39549f74bce7
legg2710 <luisemilio2710@gmail.com>
2021-06-19 13:11:14 +0000

    counter set 2

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 
 export function Home() {
-	const [count, setCount] = useState(3);
+	const [count, setCount] = useState(0);
 	return (
 		<div className="text-center mt-5">
 			<h1>{count}</h1>
-			<button onClick={() => setCount(666)}>Aumentar Contador</button>
+			<button onClick={() => setCount(count + 1)}>
+				Aumentar Contador
+			</button>
 		</div>
 	);
 }
