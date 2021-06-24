9fc390720e78b248b8cad25edb18a0bf23fcb233
legg2710 <luisemilio2710@gmail.com>
2021-06-24 01:19:50 +0000

    pre final

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,14 @@ import "../styles/index.scss";
 //components
 import { Home } from "./component/home.js";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { SecondsCounter } from "./component/home.js";
 
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+
+setInterval(function() {
+	ReactDOM.render(
+		<SecondsCounter seconds={counter} />,
+		document.getElementById("#app")
+	);
+	counter += 1;
+}, 1000);
