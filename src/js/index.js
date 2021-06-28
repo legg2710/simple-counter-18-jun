55029e03853b292c187509382376165797914fbb
legg2710 <luisemilio2710@gmail.com>
2021-06-28 02:04:03 +0000

    Simple Counter 99%

--- src/js/index.js
+++ src/js/index.js
@@ -5,12 +5,15 @@ import "../styles/index.scss";
 
 //components
 import { Home } from "./component/home.js";
-import { library } from "@fortawesome/fontawesome-svg-core";
+
 import { SecondsCounter } from "./component/home.js";
 
 let counter = 0;
 
-ReactDOM.render(
-	<SecondsCounter seconds={counter} />,
-	document.querySelector("#app")
-);
+setInterval(function() {
+	ReactDOM.render(
+		<SecondsCounter seconds={counter} />,
+		document.getElementById("#app")
+	);
+	counter += 1;
+}, 1000);
